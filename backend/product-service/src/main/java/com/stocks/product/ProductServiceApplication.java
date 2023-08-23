77d87f71f6734d311724a647f278ff3e0e6cec9c
smruti <srnayak165@gmail.com>
2023-08-23 15:28:25 +0530

    refactor: swagger header add

--- backend/product-service/src/main/java/com/stocks/product/ProductServiceApplication.java
+++ backend/product-service/src/main/java/com/stocks/product/ProductServiceApplication.java
@@ -1,9 +1,12 @@
 package com.stocks.product;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product-service", version = "v1", description = "Manage Products"))
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
