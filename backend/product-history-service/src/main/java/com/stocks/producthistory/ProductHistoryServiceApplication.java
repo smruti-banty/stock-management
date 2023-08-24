4b9eb10981e722ed4e82ceb39081502a0544457a
smruti <srnayak165@gmail.com>
2023-08-24 21:12:46 +0530

    create: product-history service

---
+++ backend/product-history-service/src/main/java/com/stocks/producthistory/ProductHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.producthistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductHistoryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/product-history-service/src/test/java/com/stocks/producthistory/ProductHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocks.producthistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductHistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
