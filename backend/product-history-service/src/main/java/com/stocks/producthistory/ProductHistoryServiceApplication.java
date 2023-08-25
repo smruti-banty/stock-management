700d4ac811b9225a96983ab42386b7058d01a813
smruti <srnayak165@gmail.com>
2023-08-25 13:24:28 +0530

    refactor: Task executor add

--- backend/product-history-service/src/main/java/com/stocks/producthistory/ProductHistoryServiceApplication.java
+++ backend/product-history-service/src/main/java/com/stocks/producthistory/ProductHistoryServiceApplication.java
@@ -2,12 +2,25 @@ package com.stocks.producthistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 public class ProductHistoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductHistoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductHistoryServiceApplication.class, args);
+    }
 
+    @Bean
+    public TaskExecutor taskExecutor() {
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
 }
