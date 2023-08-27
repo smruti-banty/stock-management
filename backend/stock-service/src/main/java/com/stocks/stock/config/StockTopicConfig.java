1133c63113fd82779472700a43ef34c4f05a6615
smruti <srnayak165@gmail.com>
2023-08-27 10:19:04 +0530

    refactor: kafka logic add

---
+++ backend/stock-service/src/main/java/com/stocks/stock/config/StockTopicConfig.java
@@ -0,0 +1,21 @@
+package com.stocks.stock.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class StockTopicConfig {
+    @Value("${kafka.topic.product.update}")
+    private String PRODUCT_UPDATE_TOPIC;
+
+    @Value("${kafka.topic.stock.add}")
+    private String PRODUCT_STOCK_TOPIC;
+
+    @Bean
+    public NewTopic productStockTopic() {
+        return TopicBuilder.name(PRODUCT_STOCK_TOPIC).build();
+    }
+}
