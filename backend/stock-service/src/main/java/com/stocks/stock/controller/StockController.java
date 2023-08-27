66721e2294cacae687834492792cac3f7b2b74b0
smruti <srnayak165@gmail.com>
2023-08-27 09:41:36 +0530

    refactor: controller layer add

---
+++ backend/stock-service/src/main/java/com/stocks/stock/controller/StockController.java
@@ -0,0 +1,70 @@
+package com.stocks.stock.controller;
+
+import com.stocks.stock.model.Stock;
+import com.stocks.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/stock")
+@RequiredArgsConstructor
+public class StockController {
+    private final StockService stockService;
+
+    @PutMapping("/{stockId}/{quantity}")
+    public void updateStock(@PathVariable String stockId, @PathVariable int quantity) {
+        stockService.updateStock(stockId, quantity);
+    }
+
+    @PutMapping("/product/{productId}/{quantity}")
+    public void updateProductStock(@PathVariable String productId, @PathVariable int quantity) {
+        stockService.updateProductStock(productId, quantity);
+    }
+
+    @PutMapping("/increase/{stockId}/{quantity}")
+    public void increaseStock(@PathVariable String stockId, @PathVariable int quantity) {
+        stockService.increaseStock(stockId, quantity);
+    }
+
+    @PutMapping("/product/increase/{productId}/{quantity}")
+    public void increaseProductStock(@PathVariable String productId, @PathVariable int quantity) {
+        stockService.increaseProductStock(productId, quantity);
+    }
+
+    @PutMapping("/decrease/{stockId}/{quantity}")
+    public void decreaseStock(@PathVariable String stockId, @PathVariable int quantity) {
+        stockService.decreaseStock(stockId, quantity);
+    }
+
+    @PutMapping("/product/decrease/{productId}/{quantity}")
+    public void decreaseProductStock(@PathVariable String productId, @PathVariable int quantity) {
+        stockService.decreaseProductStock(productId, quantity);
+    }
+
+    @GetMapping("/{stockId}")
+    public Stock getStock(@PathVariable String stockId) {
+        return stockService.getStock(stockId);
+    }
+
+    @GetMapping("/product/{productId}")
+    public Stock getProductStock(@PathVariable String productId) {
+        return stockService.getProductStock(productId);
+    }
+
+    @GetMapping
+    public List<Stock> getStocks() {
+        return stockService.getStocks();
+    }
+
+    @GetMapping("/product/active")
+    public List<Stock> getActiveStocks() {
+        return stockService.getActiveProductsStocks();
+    }
+
+    @GetMapping("/product/active")
+    public List<Stock> getInActiveStocks() {
+        return stockService.getInActiveProductsStocks();
+    }
+}
