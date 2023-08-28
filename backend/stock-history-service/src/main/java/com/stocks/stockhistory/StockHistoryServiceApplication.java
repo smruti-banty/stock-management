cf8a978e64a4646a9d3561502b7fda5fb0ca090e
smruti <srnayak165@gmail.com>
2023-08-28 17:49:06 +0530

    stock history service add

---
+++ backend/stock-history-service/src/main/java/com/stocks/stockhistory/StockHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.stockhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockHistoryServiceApplication.class, args);
+	}
+
+}
